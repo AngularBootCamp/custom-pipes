d09fb977f223d08c772ed5867aba609baf236d6a
Kyle Cordes <kyle.cordes@oasisdigital.com>
2018-08-08 11:54:20 -0500

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ToCapsPipe, ContainsXPipe, CheckmarkPipe, FieldRangePipe } from './pipes';
+import { CheckmarkPipe, ContainsXPipe, FieldRangePipe, ToCapsPipe } from './pipes';
 
 @NgModule({
   declarations: [
